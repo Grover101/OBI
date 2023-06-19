e928f39a2d77cc97cbb6f8024d3aaf0ba9401333
Grover101 <limachigrover09@gmail.com>
2023-06-18 21:54:12 -0400

    add variables y tipos de datos

---
+++ variables.cpp
@@ -0,0 +1,44 @@
+// un libreria
+#include <bits/stdc++.h> // esta libreria tiene las librerias necesarias para la competencia
+
+using namespace std;
+
+// funcion principal
+int main()
+{
+    /**
+     * tipos de datos
+     *
+     * int // para numero enteros: 1 0 -1 12345678
+     * long long // es para numeros enteros: 1234567897865654
+     * float // nos sirve para numero decimales: 15.5 6.59 -5.6   || 123456.12356
+     * double // es para decimales mas grandes
+     * char // es para un caracter: '5' 's' '-' || esto siempre son uno
+     * string // un array de caracters ( char ) || "hola" "4568asdf" "como estas!"
+     * boolean // estos son valores de falso o verdadero || 0 o 1 || false o true
+     *
+     */
+
+    /**
+     * Declaracion de Varibles o datos
+     *
+     * (tipo de dato) (nombre de la variable) = ( valor ) // opcional;
+     *
+     *  int numero = 5;
+     *
+     */
+
+    // int s = 3.8 + 5.9;
+    // cout << s << endl;
+    // float numero = 4.5 + s;
+    // cout << numero << endl;
+    // numero = 5;
+    // cout << numero << endl;
+
+    // char caracter = '2';
+    // string cadena = "hola";
+
+    // cout << (cadena[1]) << endl;
+
+    return 0;
+}
