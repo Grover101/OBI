8e27bf627a9e741b1e8eb1723f0e39d7feaae6c6
Grover101 <limachigrover09@gmail.com>
2023-07-01 15:42:08 -0400

    add coment push, top, pop

--- pilas.cpp
+++ pilas.cpp
@@ -12,11 +12,14 @@ int main()
     // declarar
     stack<int> pila;
 
+    // push -> para introducir a la pila
     pila.push(4);
     pila.push(41);
     pila.push(20);
     pila.push(15);
 
+    // top -> es para ver el ultimo elemento de la pila
+    // pop -> elimina el ultimo elemento en la pila
     int ul = pila.top();
     pila.pop();
     cout << ul << endl;
